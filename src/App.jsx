25bbb2aaf2182d1e4a7a9b87837aa5220be02968
M Ridhwan Fadhli <rayhanarkan30@gmail.com>
2023-04-02 23:47:26 +0700

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
-    </Router>
+    // <Router basename>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/product" element={<Product />} />
+    //   </Routes>
+    // </Router>
+    <>
+      <Home />
+    </>
   );
 };
 
