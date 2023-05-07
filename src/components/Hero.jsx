f4e0821267d180becf0785ed106ddda6fcd7bc6b
Mohammad Rayhan Arkan Fahlevi <pahle@Mohammads-MacBook-Air.local>
2023-05-07 09:30:06 +0700

    video fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
 
         </div> */}
         <video
-          src="./video/hero.mp4"
+          src="./video/hero.MP4"
           className="w-[646px] h-full object-cover rounded-xl"
           autoPlay
           loop
