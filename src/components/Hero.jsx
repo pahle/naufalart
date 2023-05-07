3f9873def4daed4115e2b27ead429a3ee808c462
Mohammad Rayhan Arkan Fahlevi <pahle@Mohammads-MacBook-Air.local>
2023-05-07 09:42:22 +0700

    dummy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
               Living with nature every day
             </h1>
         </div>
-        <a className="w-[150px]" href="#benefits">
+        {/* <a className="w-[150px]" href="#benefits">
           <div className="border-2 border-secondary rounded-lg p-2">
             <div className="border-2 border-secondary rounded-lg p-2 -mb-5 -mr-5">
               <p className="text-center mb-3 mr-2 font-poppins">
@@ -25,19 +25,19 @@ const Hero = () => {
           src="./icons/arrow1.svg"
           alt="arrow"
           className="w-20 absolute xl:bottom-2 2xl:bottom-8 -xl:hidden -left-6"
-        />
+        /> */}
       </div>
       <div className="w-full xl:h-[80vh] h-[75vh] overflow- relative">
         {/* <div className="absolute w-full h-full border-4 border-secondary rounded-xl -top-5 -right-5 z-[-10]">
 
         </div> */}
-        <video
+        {/* <video
           src="./video/hero.MP4"
           className="w-[646px] h-full object-cover rounded-xl"
           autoPlay
           loop
           muted
-        />
+        /> */}
       </div>
     </section>
   );
