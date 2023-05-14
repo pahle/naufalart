8ea623385451c487de4801316fbced0aea91dcc4
Rayhan Arkan <rayhanarkan30@gmail.com>
2023-05-14 08:20:44 +0700

    webp

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,64 @@
 import React from "react";
 
 import {
-    Navbar,
-    Hero,
-    Benefits,
-    Products,
-    About,
-    Testimonials,
-    Contact,
-    Footer,
-  } from "./components";
+  Navbar,
+  Hero,
+  Benefits,
+  Products,
+  About,
+  Testimonials,
+  Contact,
+  Footer,
+} from "./components";
 
 const App = () => {
   return (
     <>
-        <div className="bg-[#FFFCF9]/90 w-full overflow-hidden">
-      <img
-        src="./icons/lines.svg"
-        alt="lines"
-        className="absolute w-full h-full opacity-25 z-[-20] object-cover"
-      />
+      <div className="bg-[#FFFCF9]/90 w-full overflow-hidden">
+        <img
+          src="./icons/lines.svg"
+          alt="lines"
+          className="absolute w-full h-full opacity-25 z-[-20] object-cover"
+        />
 
-      <div className="flexCenter md:paddingX">
-        <div className="boxWidth">
-          <Navbar />
+        <div className="flexCenter md:paddingX">
+          <div className="boxWidth">
+            <Navbar />
+          </div>
         </div>
-      </div>
 
-      <div className="flexStart paddingX">
-        <div className="boxWidth">
+        <div className="flexStart paddingX">
+          <div className="boxWidth">
             <Hero />
+          </div>
         </div>
-      </div>
 
-      <div className="paddingX flexStart">
-        <div className="boxWidth">
+        <div className="paddingX flexStart">
+          <div className="boxWidth">
             <Benefits />
+          </div>
         </div>
-      </div>
 
-      <div className="paddingX flexStart">
-        <div className="boxWidth">
+        <div className="paddingX flexStart">
+          <div className="boxWidth">
             <Products />
+          </div>
         </div>
-      </div>
 
-      <div className="paddingX flexStart">
-        <div className="boxWidth">
+        <div className="paddingX flexStart">
+          <div className="boxWidth">
             <About />
             <Testimonials />
             <Contact />
+          </div>
         </div>
-      </div>
 
-      <div className="paddingX flexStart">
-        <div className="boxWidth">
+        <div className="paddingX flexStart">
+          <div className="boxWidth">
             <Footer />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
